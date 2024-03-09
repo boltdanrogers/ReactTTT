70062629df7f42183198ebc569ef9ba30051686a
Daniel John Rogers <69739876+boltdanrogers@users.noreply.github.com>
2024-03-08 21:40:51 -0500

    check for winner, display current player or identity of winner

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function Square({ value, onSquareClick }) {
   //remove the state from these inner components now that board is keeping track
   // const [value, setValue] = useState(null);
 
-  //take out the handleclick
+  //take out the handleClick
   /* function handleClick() {
     setValue("X");
     console.log(`clicked`);
@@ -26,13 +26,15 @@ export default function Board() {
   //and now we add a boolean state to keep track of who's turn it is
   const [xIsNext, setXIsNext] = useState(true);
   function handleClick(i) {
-    //in order to only allow a click to modify an empty square, start with a conditional
-    if (squares[i]) {
+    //in order to only allow a click to modify an empty square, start with a conditional based on the existence if the current cell
+    //if the cell is anything but null, we do not want to let the rest of the handleClick function alter the square that was clicked
+    //also we check to see if the game has been won
+    if (squares[i] || calculateWinner(squares)) {
       return;
     }
     //make a copy of the squares array with the slice method
     const nextSquares = squares.slice();
-    //confitional branching *wink*
+    //conditional branching *wink*
     if (xIsNext) {
       nextSquares[i] = "X";
     } else {
@@ -51,9 +53,22 @@ export default function Board() {
 
   //we can't just have onSquareClick={handleClick(0), this will lead to an infinite loop because it is called right away
   //instead, we can define a function to be passed, but in this case we will use arrow functions to save time and space
+  //create a const winner to hold the return of calculating the winner
+  const winner = calculateWinner(squares);
+  //create the variable to hold status
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next Player: " + (xIsNext ? "X" : "O");
+  } //end of if then else
+
+  //add a a status section above the board to display the winner
 
   return (
     <>
+      <div className="status">{status}</div>
+
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -73,4 +88,34 @@ export default function Board() {
       </div>
     </>
   );
-}
+} //end of Board
+
+function calculateWinner(squares) {
+  //an array of arrays, each with an array of winning lines
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  //now we test if the current state of squares matches any of the winning lines
+
+  for (let i = 0; i < lines.length; i++) {
+    //use destructuring to assign a b and c the three variables from the current inner array
+    const [a, b, c] = lines[i];
+    //now test to see if the current square completes the current winning line
+    //test the existence of the first element, and then that the first and second and first and third all match
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      //return the winning character if the current winning line matches the state of the squares array
+      return squares[a];
+    } //end of if
+  } //end of for i loop
+
+  //if the for loop has not resulted in a match that returned the winning character, get here and return null
+
+  return null;
+} //end of calculate winner
